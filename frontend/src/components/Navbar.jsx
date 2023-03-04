b25c0f4f32de7f4bc0963fc7ba355b758b1455bf
DarkHumors <redtornado205@gmail.com>
2023-03-05 00:23:34 +0800

    last last

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Cookies from "js-cookie";
 
 const Navbar = () => {
   return (
     <div className="fixed h-20 w-full bg-gray-900 flex items-center p-4 justify-between">
       <span className="text-white font-Roboto text-[25px]">INVOICE TEST</span>
-      <span className="text-white text-[18px] cursor-pointer rounded-md p-1 hover:(bg-gray-600)">
+      <span
+        className="text-white text-[18px] cursor-pointer rounded-md p-1 hover:(bg-gray-600)"
+        onClick={() => Cookies.remove("user_token")}
+      >
         Logout
       </span>
     </div>
